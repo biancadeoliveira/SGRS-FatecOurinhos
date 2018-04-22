9f913fa1b26ef36d09105cc29fb13b0882daebde
LucasGabrielSouza <37641124+LucasGabrielSouza@users.noreply.github.com>
2018-04-22 15:28:10 -0300

    View Item

---
+++ app/system/Views/formItem.php
@@ -0,0 +1,32 @@
+<div class="content">
+
+	<fieldset>
+	   <legend>Cadastrar Item</legend>
+	   
+	   <style type="text/css">
+	   	 td {
+	   	 	float: left;
+
+	   	 }
+	   </style>
+	
+		<form method="POST" id="form" action="<?php echo ('http://localhost/sgrs/public/painel/item')?>">
+			
+			<table>
+				<tr>
+					
+					<td><label>Código do Pedido</label><br><input type="number" name="codPedido"></td>
+					<td><label>Código do Produto</label><br><input type="number" name="codProduto"></td>
+					<td><label>Quantidade</label><br><input type="number" name="quantidade"></td>
+					<td><label>Preço</label><br><input type="text" name="preco"></td>
+					<td><label>Estado</label><br><input type="text" name="estado"></td>
+					<td><label>Observação</label><br><input type="text" name="observacao"></td>
+
+					<td style="padding: 15px;"><input type="button" value="Inserir" style="height: 100%;" onclick="confirmarExclusao('Confirmar cadastro?');"></td>	
+					</tr>
+			</table>
+
+		</form>
+
+	</fieldset>
+</div>
