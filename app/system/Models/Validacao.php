1b7df8b4e88cd5b15cb757dd40188ef4f00bdf18
Lucas <devlucasmendonca@gmail.com>
2018-04-25 20:36:27 -0300

    implementação do metodo de validar cpf

--- app/system/Models/Validacao.php
+++ app/system/Models/Validacao.php
@@ -49,4 +49,31 @@ class Validacao
 		}
 	}
 
+
+	/*
+	** Author: Lucas de Abreu Mendonça e Gabriel Ribeiro
+	** Date: 2018-04-25
+	**
+	** Validação CPF
+	*/
+
+	public static function validar_cpf($cpf) {
+
+		$cpf = preg_replace('/[^0-9]/', '', (string) $cpf);
+		// Valida tamanho
+		if (strlen($cpf) != 11)
+			return false;
+		// Calcula e confere primeiro dígito verificador
+		for ($i = 0, $j = 10, $soma = 0; $i < 9; $i++, $j--)
+			$soma += $cpf{$i} * $j;
+		$resto = $soma % 11;
+		if ($cpf{9} != ($resto < 2 ? 0 : 11 - $resto))
+			return false;
+		// Calcula e confere segundo dígito verificador
+		for ($i = 0, $j = 11, $soma = 0; $i < 10; $i++, $j--)
+			$soma += $cpf{$i} * $j;
+		$resto = $soma % 11;
+		return $cpf{10} == ($resto < 2 ? 0 : 11 - $resto);
+	}
+
 }
