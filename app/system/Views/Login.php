0912703f1699c996e065c2a33465db9fc0aa3a14
Lucas <devlucasmendonca@gmail.com>
2018-05-01 13:35:13 -0300

    Alterei o login.php e crie o css de login

--- app/system/Views/Login.php
+++ app/system/Views/Login.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="http://localhost/framework/public/assets/login.css">
+	<title>SGRS - Login</title>
+	<link rel="stylesheet" type="text/css" href="http://localhost/SGRS-FatecOurinhos/public/assets/login.css">
 </head>
 <body>
 
-	<h2>
-		Você esta em login
-	</h2>	
-		<form method='post' action='http://localhost/framework/public/app/login'>
+	<div class="login">
+	<h1>
+		Login
+	</h1>	
+		<form method='post' action='http://localhost/SGRS-FatecOurinhos/public/app/login'>
 			<input type='text' name='login' placeholder='CPF: xxx.xxx.xxx-xx'><br>
 			<input type='password' name='senha' placeholder='Senha'><br>
-			<input type='submit' value='Entrar'>
+			<input type='submit' value='Entrar' class="btn btn-primary btn-block btn-large">
 		</form>
+
+	</div>
 </body>
 </html>
