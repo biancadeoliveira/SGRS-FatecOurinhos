d5b09a65b7de0cafaade51dce9fc5cbe917c5b45
Lucas <devlucasmendonca@gmail.com>
2018-06-19 22:22:11 -0300

    configuracao geral da home

--- app/system/Views/home.php
+++ app/system/Views/home.php
@@ -1,34 +1,99 @@
-<div class="dados">
+<div class="dados col-4">
 	<div class="col-2">
 		<div class="chamada-home">
-			<div class="media"></div>
-			Usuários
+			<a href="<?php echo $GLOBALS['$urlpadrao']."painel/enderecos"  ?>">
+				<img class="homeimg" src="<?php echo $GLOBALS['$urlImg']."homeicon/homeendereco.png"  ?>">
+			</a>
+			Endereços
 		</div>
-		
+
 	</div>
 	<div class="col-2">
 		<div class="chamada-home">
-			Cardapio
+			<a href="<?php echo $GLOBALS['$urlpadrao']."painel/usuario"  ?>">
+				<img class="homeimg" src="<?php echo $GLOBALS['$urlImg']."homeicon/homeusuario.png"  ?>">
+			</a>
+			Usuário
 		</div>
 	</div>
 	<div class="col-2">
 		<div class="chamada-home">
+			<a href="<?php echo $GLOBALS['$urlpadrao']."painel/clientes"  ?>">
+				<img class="homeimg" src="<?php echo $GLOBALS['$urlImg']."homeicon/homecliente.png"  ?>">
+			</a>
+			Clientes
+		</div>
+	</div>
+	<div class="col-2">
+		<div class="chamada-home">
+			<a href="<?php echo $GLOBALS['$urlpadrao']."painel/produtos"  ?>">
+				<img class="homeimg" src="<?php echo $GLOBALS['$urlImg']."homeicon/homecardapio.png"  ?>">
+			</a>
+			Cardápio
+		</div>
+	</div>
+	<div class="col-2">
+		<div class="chamada-home">
+			<a href="<?php echo $GLOBALS['$urlpadrao']."painel/mesas"  ?>">
+				<img class="homeimg" src="<?php echo $GLOBALS['$urlImg']."homeicon/homemesa.png"  ?>">
+			</a>
 			Mesas
 		</div>
 	</div>
 	<div class="col-2">
 		<div class="chamada-home">
+			<a href="<?php echo $GLOBALS['$urlpadrao']."painel/reservas"  ?>">
+				<img class="homeimg" src="<?php echo $GLOBALS['$urlImg']."homeicon/homereserva.png"  ?>">
+			</a>
 			Reservas
 		</div>
 	</div>
 	<div class="col-2">
 		<div class="chamada-home">
+			<a href="<?php echo $GLOBALS['$urlpadrao']."painel/pedidos"  ?>">
+				<img class="homeimg" src="<?php echo $GLOBALS['$urlImg']."homeicon/homepedido.png"  ?>">
+			</a>
 			Pedidos
 		</div>
 	</div>
 	<div class="col-2">
 		<div class="chamada-home">
+			<a href="<?php echo $GLOBALS['$urlpadrao']."painel/pagamentos"  ?>">
+				<img class="homeimg" src="<?php echo $GLOBALS['$urlImg']."homeicon/homepagamento.png"  ?>">
+			</a>
 			Pagamentos
 		</div>
+	</div>
+	<div class="col-2">
+		<div class="chamada-home">
+			<a href="<?php echo $GLOBALS['$urlpadrao']."painel/relatorios"  ?>">
+				<img class="homeimg" src="<?php echo $GLOBALS['$urlImg']."homeicon/homerelatorio.png"  ?>">
+			</a>
+			Relatório
+		</div>
+	</div>
+
+</div>
+<div class="dados col-2">
+	<div class="hometexto">
+		<h2>SGRS</h2>
+		<br>
+		<p>Sistema de Gerenciamento de Restaurantes e Similares (SGRS), apresentado a Faculdade de Tecnologia de Ourinhos, como exigência parcial à aprovação na Disciplina Laboratório de Engenharia de Software.</p>
+		<br>
+		Profª Dra. Elaine Pasqualini
+		<br>
+		<br>
+		<br>
+		<br>
+		<h4>Alunos:</h4><br>
+		Bianca Souza de Oliveira<br>
+		Gabriel Cunha Ribeiro<br>
+		Gustavo Luiz Barbosa da Cunha<br>
+		Lucas Candido Granado<br>
+		Lucas de Abreu Mendonça<br>
+		Lucas Gabriel de Souza<br>
+
+
+
 	</div>
 </div>
